851a6b5db243548cc34b9aa7acaa7e554cdd9634
Ville Jestila <t9jevi00@students.oamk.fi>
2021-08-16 02:31:42 +0300

    asd

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const morgan = require('morgan')
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
 
 morgan.token('body', function (req, res) {
     return[
